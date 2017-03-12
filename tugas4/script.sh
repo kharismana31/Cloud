c0eedb5aacbe7f7fd7a9ebc19c0e117f491553ea
kharismana31 <kharismana31@gmail.com>
2017-03-13 02:28:53 +0700

    sukses install ruby on rails using script

--- tugas4/script.sh
+++ tugas4/script.sh
@@ -1,21 +1,17 @@
 #!/usr/bin/env bash
-#sudo apt-get update
-#sudo apt-get -y install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
-#git clone https://kharismana31:aptx4869@github.com/rbenv/rbenv.git .rbenv
-#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/ubuntu/.bashrc
-#echo 'eval "$(rbenv init -)"' >> /home/ubuntu/.bashrc
-#source /home/ubuntu/.bashrc
-#git clone https://kharismana31:aptx4869@github.com/rbenv/ruby-build.git /home/ubuntu/.rbenv/plugins/ruby-build
-#chmod 777 .rbenv
-#sudo -H -u ubuntu bash -i -c 'rbenv install 2.3.1'
-#sudo -H -u ubuntu bash -i -c 'rbenv global 2.3.1'
-#echo "gem: --no-document" > /home/ubuntu/.gemrc
-#sudo -H -u ubuntu bash -i -c 'gem install bundler'
-#sudo -H -u ubuntu bash -i -c 'gem install rails'
-#sudo -H -u ubuntu bash -i -c 'rbenv rehash'
-#cd /tmp
-#\curl -sSL https://deb.nodesource.com/setup_6.x -o nodejs.sh
-#less nodejs.sh
-#cat /tmp/nodejs.sh | sudo -E bash -
-#sudo apt-get install -y nodejs
-sudo -H -u ubuntu bash -i -c 'rails new rubyonrails'
+
+gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3
+cd /tmp
+curl -sSL https://get.rvm.io -o rvm.sh
+cat /tmp/rvm.sh | bash -s stable --rails
+source /home/ubuntu/.rvm/scripts/rvm
+rvm install 2.3.0
+gem install rails
+\curl -sSL https://deb.nodesource.com/setup_6.x -o nodejs.sh
+cat /tmp/nodejs.sh | sudo -E bash -
+sudo apt-get update
+sudo apt-get install -y nodejs
+cd /home/ubuntu
+rails new Test
+cd Test
+rails server
