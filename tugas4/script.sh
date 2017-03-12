25df807d878f7858925af351083af642ad2b76ea
danielfablius <danielfablius@gmail.com>
2017-03-12 21:03:12 +0700

    nomor 4

---
+++ tugas4/script.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+#sudo apt-get update
+#sudo apt-get -y install autoconf bison build-essential libssl-dev libyaml-dev libreadline6-dev zlib1g-dev libncurses5-dev libffi-dev libgdbm3 libgdbm-dev
+#git clone https://kharismana31:aptx4869@github.com/rbenv/rbenv.git .rbenv
+#echo 'export PATH="$HOME/.rbenv/bin:$PATH"' >> /home/ubuntu/.bashrc
+#echo 'eval "$(rbenv init -)"' >> /home/ubuntu/.bashrc
+#source /home/ubuntu/.bashrc
+#git clone https://kharismana31:aptx4869@github.com/rbenv/ruby-build.git /home/ubuntu/.rbenv/plugins/ruby-build
+#chmod 777 .rbenv
+#sudo -H -u ubuntu bash -i -c 'rbenv install 2.3.1'
+#sudo -H -u ubuntu bash -i -c 'rbenv global 2.3.1'
+#echo "gem: --no-document" > /home/ubuntu/.gemrc
+#sudo -H -u ubuntu bash -i -c 'gem install bundler'
+#sudo -H -u ubuntu bash -i -c 'gem install rails'
+#sudo -H -u ubuntu bash -i -c 'rbenv rehash'
+#cd /tmp
+#\curl -sSL https://deb.nodesource.com/setup_6.x -o nodejs.sh
+#less nodejs.sh
+#cat /tmp/nodejs.sh | sudo -E bash -
+#sudo apt-get install -y nodejs
+sudo -H -u ubuntu bash -i -c 'rails new rubyonrails'
